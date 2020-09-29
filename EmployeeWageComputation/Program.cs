8d8dca315e4f9d85385024b36dcccf70df1728fe
piyush12346789 <piyushgupta024@gmail.com>
2020-09-29 12:05:11 +0530

    [Piyush] Refactor: Part Time Employee Wage.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,16 +7,19 @@ namespace EmployeeWageComputation
         {
             Console.WriteLine("Welcome to Employee Wage Computation.");
             int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is absent");
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
-            if (empCheck == IS_FULL_TIME)
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is absent");
+            if (empCheck == IS_PART_TIME)
+                empHrs = 4;
+            else if (empCheck == IS_FULL_TIME)
                 empHrs = 8;
             else
                 empHrs = 0;
