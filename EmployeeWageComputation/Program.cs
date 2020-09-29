3b2801700527f48796e296426c99c707124e0721
piyush12346789 <piyushgupta024@gmail.com>
2020-09-29 12:01:17 +0530

    [Piyush] Refactor: Daily Employee Wage.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -13,6 +13,15 @@ namespace EmployeeWageComputation
                 Console.WriteLine("Employee is Present");
             else
                 Console.WriteLine("Employee is absent");
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
