6a6434cca1a034034154daddfd36d4c13bb16bf3
piyush12346789 <piyushgupta024@gmail.com>
2020-09-29 15:41:55 +0530

    [Piyush] Refactor: Employee wage calculation using Class method.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,36 +5,47 @@ namespace EmployeeWageComputation
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
+        public const int EMP_WAGE_PER_HR = 20;
         public const int NUMBER_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
+        public static int empHrs = 0;
+        public static int totalEmpHrs = 0;
+        public static int totalWorkingDays = 0;
+        public static int GetWorkingHour(int ran)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation.");
             int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
+            switch (ran)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+                }
+            return empHrs;
+        }
+        public static void ComputeEmployeeWage()
+        {
+            int monthlySalary = 0;
+            while(totalWorkingDays < MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
+                int ran = (random.Next() % 3) + 1;
+                empHrs = GetWorkingHour(ran);
+                totalEmpHrs = totalEmpHrs + empHrs;
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage is :" + totalEmpWage);
+            monthlySalary = EMP_WAGE_PER_HR * totalEmpHrs;
+            Console.WriteLine("Total salary of employee in a month is " + monthlySalary);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation.");
+            ComputeEmployeeWage();
         }
     }
 }
