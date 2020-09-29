db9de67e50f89821958db6e356964a3c86cfe666
piyush12346789 <piyushgupta024@gmail.com>
2020-09-29 11:55:58 +0530

    [Piyush] Refactor: Employee Attendance.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,6 +6,13 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation.");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is absent");
         }
     }
 }
